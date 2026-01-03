1466818b5830bd66c8179d18d006107a784a320e
pratik gt <n.pratikgt12@gmail.com>
2026-01-03 07:17:39 +0545

    'removed_all_unneccesaryitems'

--- pratmandu/lib/app.dart
+++ pratmandu/lib/app.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-import 'package:pratmandu/screens/splash_screen.dart';
+import 'package:pratmandu/features/splash/presentation/pages/splash_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
-  /////globallly theme set gareko ////////
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
-      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+      title: 'Pratmandu',
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const SplashPage(),
     );
   }
 }
